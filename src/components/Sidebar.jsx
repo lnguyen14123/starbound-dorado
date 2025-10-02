ea587d404611f2d754a44b32371a15c93c740276
Luc Nguyen <lucnguyen1416@gmail.com>
2025-10-02 09:58:18 -0700

    added side rectangle

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Sidebar({ tabs, currentTab, onTabClick }) {
+  return (
+    <div className="w-80 bg-transparent text-white h-screen grid items-center">
+    <div className="w-65 h-[85vh] bg-[#d6ae92] border-8 border-[#524136] border-l-0 rounded-sm -ml-4"></div>
+    </div>
+  );
+}
+
+export default Sidebar;
